632570ced3822bb93b036311bb3f1983acca09b6
Quang-Linh LE <linktohack@gmail.com>
2016-12-05 15:30:09 +0100

    Update Model.swift

--- Model.swift
+++ Model.swift
@@ -62,11 +62,11 @@ class Model {
         
         return Promise<Model>(resolvers: { (resolve, reject) in
             Alamofire.request(parameterizedUrl!, method: m, parameters: parameters).responseJSON(completionHandler: {res in
-                if let json = res.result.value as? [String: Any?] {
-                    let _ = self.fromJSON(json)
-                    resolve(self)
+                if res.result.isFailure {
+                    reject (res.result.error!)
                 } else {
-                    reject("Error" as! Error)
+                    let _ = self.fromJSON(res.result.value! as! [String : Any?])
+                    resolve(self)
                 }
             })
         })
@@ -77,21 +77,23 @@ class Model {
     func patch() -> Promise<Model> { return sync(method: .patch) }
     func delete() -> Promise<Model> { return sync(method: .delete) }
     func read() -> Promise<Model> { return sync(method: .read) }
-
+    
     
     static func fetch(url: String?, params: [String: String]?) -> Promise<[Model]> {
         let parameterizedUrl = params?.reduce(url!, { (acc: String, it: (key: String, value: String)) -> String in
             return acc.replacingOccurrences(of: [":", it.key].joined(), with: "")
-        })
+        }).replacingOccurrences(of: ":_id", with: "")
         
         return Promise<[Model]>(resolvers: { (resolve, reject) in
             Alamofire.request(parameterizedUrl!, method: .get).responseJSON(completionHandler: {res in
-                if let json = res.result.value as? [[String: Any?]] {
-                    resolve(json.map({ self.init(url: url, params: params).fromJSON($0) }))
+                if res.result.isFailure {
+                    reject (res.result.error!)
                 } else {
-                    reject("Error" as! Error)
+                    resolve((res.result.value as! [[String: Any?]]).map({ self.init(url: url, params: params).fromJSON($0) }))
                 }
             })
         })
     }
 }
+
+
